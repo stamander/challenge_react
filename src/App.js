0af64eed77e5f2c3919701e362d1e8d604b7b600
stamander <iida.atsushi.fh@gmail.com>
2020-05-24 11:15:10 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
+// Reactをインポートしてください
 import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>h1です</h1>
-        <h2>h2です</h2>
-        <h3>h3です</h3>
-
-        <img src = "logo.svg"/>
-         
-        {/* <p>pです</p> */}
-      </div>
+// Appクラスを定義してください
+class App extends React.Component{
+  render(){
+   return (
+      <h1>Hello React</h1>
     );
   }
 }
 
+// Appクラスをエクスポートしてください
 
 export default App;
