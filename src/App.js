74bbafd0f499ab901d87bbd479a7d42bf5c42b62
stamander <iida.atsushi.fh@gmail.com>
2020-05-24 15:28:57 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 // Appクラスを定義してください
 class App extends React.Component{
   render(){
-    const text = 'Hello world'
+    const text = 'Hello world';
    return (
      
       <div> {text} 
