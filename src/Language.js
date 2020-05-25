9a5828ffcd83bedd9f0122ea237cfada372ac002
stamander <iida.atsushi.fh@gmail.com>
2020-05-25 17:45:34 +0900

    Update Language.js

--- src/Language.js
+++ src/Language.js
@@ -3,6 +3,7 @@ import React from 'react';
 class Language extends React.Component{
 
   render(){
+    console.log(this.props);
     return(
       <div className='language-item'>
         <div className='language-name'>HTML & CSS</div>
